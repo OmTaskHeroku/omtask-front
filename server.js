013422535b4ba6643fdff4da67dbc5d6a79cbdec
ulpioro <78817508+ulpioro@users.noreply.github.com>
2022-04-26 14:19:27 +0200

    Added some staff

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
+const history = require('connect-history-api-fallback')
+let app = express();
+
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
