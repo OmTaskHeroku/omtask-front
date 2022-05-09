1435b1d844be16f7c14308b7875e52f0785a143e
XFenrirQ <krystiankisielewski0@gmail.com>
2022-05-09 14:25:35 +0200

    Manu zamienione na typ dropdown

--- src/components/NawigacjaBar.vue
+++ src/components/NawigacjaBar.vue
@@ -1,22 +1,17 @@
 <template>
   <nav class="navbar navbar-expand navbar-light fixed-top">
-    <div class="container">
+    <aside class="menu">
       <a href="/" class="navbar-brand">Home</a>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a href="/registration" class="nav-link">Registration</a>
-          </li>
-          <li class="nav-item">
-            <a href="/login" class="nav-link">Login</a>
-          </li>
-          <li class="nav-item">
-            <a href="/profile" class="nav-link">Profile</a>
-          </li>
-
-        </ul>
+      <div class="dropdown">
+        <button class="dropbtn">Menu</button>
+        <div class="dropdown-content">
+          <a href="/">Home</a>
+          <a href="/profile">Profile</a>
+          <a href="/registration">Register</a>
+          <a href="/login">Login</a>
+        </div>
       </div>
-    </div>
+    </aside>
   </nav>
 </template>
 
@@ -27,5 +22,44 @@ export default {
 </script>
 
 <style scoped>
+/* Dropdown Button */
+.dropbtn {
+  color: #000000;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
 
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #ddd;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {display: block;}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {background-color: #486fea;}
 </style>
